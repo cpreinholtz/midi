7e5fea282d6d5fcf6fa6c2664eb2c396d173d0d9
Clark <cpr978@gmail.com>
2020-12-26 18:37:24 -0800

    add checks to hopefully do exactly 24 midi out pulses per clk pulse

--- midiclk/midiclk.ino
+++ midiclk/midiclk.ino
@@ -1,23 +1,24 @@
 /*
-  MIDI note player
+Written by Clark using various sources of inspiration.  Use at your own risk.
 
-  This sketch shows how to use the serial transmit pin (pin 1) to send MIDI note data.
-  If this circuit is connected to a MIDI synth, it will play the notes
-  F#-0 (0x1E) to F#-5 (0x5A) in sequence.
-
-  The circuit:
-  - digital in 1 connected to MIDI jack pin 5
-  - MIDI jack pin 2 connected to ground
-  - MIDI jack pin 4 connected to +5V through 220 ohm resistor
-  - Attach a MIDI cable to the jack, then to a MIDI synth, and play music.
-
-  created 13 Jun 2006
-  modified 13 Aug 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Midi
+Pulse in, detect rising edge
+Midi clock out with 24 times faster period per midi spec 
+  
+  
+  Circuit:
+  
+    Pulse interface (TS 3.5mm)
+        Arduino pin 2      <- Pulse TIP
+        Arduino GND         <- Pulse Sleeve
+        
+    MIDI interface (TRS 3.5mm)
+        Arduino Tx pin 1-> 220ohm resistor -> MIDI TIP
+        Arduino 5v        -> 220ohm resistor -> MIDI Ring
+        Arduino GND         -> MIDI Sleeve
+    
+    I then used a TRS to MIDI cable to get the midi to my synth!
+    
+  
 */
 
 #define MIDI_SYSRT_CLK 0xF8
@@ -27,86 +28,83 @@
 
 
 void setup() {
-  // Set MIDI baud rate:
-  Serial.begin(31250);
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(PULSE_PIN, INPUT);
+    // Set MIDI baud rate:
+    Serial.begin(31250);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(PULSE_PIN, INPUT);
 
 }
 
 void loop() {
 
-  do_without_delay();
-  getPulse();
-  
+    do_without_delay();
+    getPulse();
+    
 }
 
-int pulseLastState = 0;
+int pulseLastState = LOW;
 
 unsigned long pulsePeriodMicros = 0;
 unsigned long pulseEdgeLastMicros = 0;
-
+//const unsigned long pulseDebounceMicros = 10;
 
 unsigned long midiPeriodMicros = 2000000/24;
 unsigned long midiEdgeLastMicros = 0;
 
 const int midiDivider = 24;           // interval at which to blink (milliseconds)
 
-//int midiLeftThisPulse = midiDivider;
+int midiLeftThisPulse = midiDivider;
 
 
 void getPulse(){
 
-  int pulseState = digitalRead(PULSE_PIN);
-  //pulseState = LOW; //debug only
-  digitalWrite(LED_BUILTIN, pulseState);   // turn the LED on (HIGH is the voltage level)
-  if (pulseState == HIGH and pulseLastState == LOW) {
-    unsigned long currentMicros = micros();
-    pulsePeriodMicros = currentMicros - pulseEdgeLastMicros;
-    pulseEdgeLastMicros = currentMicros;    
-    midiPeriodMicros = pulsePeriodMicros / midiDivider;
-    do_now();
-  }  
+    int pulseState = digitalRead(PULSE_PIN);
+    //pulseState = LOW; //debug only
+    digitalWrite(LED_BUILTIN, pulseState);     // turn the LED on (HIGH is the voltage level)
+    if (pulseState == HIGH and pulseLastState == LOW) {
+        unsigned long currentMicros = micros();
+        pulsePeriodMicros = currentMicros - pulseEdgeLastMicros;
+        pulseEdgeLastMicros = currentMicros;        
+        midiPeriodMicros = pulsePeriodMicros / midiDivider;
+        do_remianing();
+        do_now(currentMicros);
+    }    
 
-  pulseLastState = pulseState;
-  
+    pulseLastState = pulseState;
+    
+}
+
+
+//hopefully we never get here
+void do_remianing(){
+    for (; midiLeftThisPulse>0; midiLeftThisPulse --){
+         Serial.write(MIDI_SYSRT_CLK);
+    } 
 }
 
 
 
-
-
-
-void do_now (){
-  
-  unsigned long currentMicros = micros();
-  //if (currentMicros - midiEdgeLastMicros >= midiPeriodMicros) {
+void do_now(unsigned long currentMicros){
+    Serial.write(MIDI_SYSRT_CLK);
+    
     // save the last time you blinked the LED
     midiEdgeLastMicros = currentMicros;
-    //do something here
-    Serial.write(MIDI_SYSRT_CLK);
-    //Serial.println("currentMicros");
-    //Serial.println("MIDI OUT NOW !!!!!!!!!!!!!!!");
-
-    //Serial.println(currentMicros);
-  //}
+  
+    midiLeftThisPulse = midiDivider-1;
+  
 
 }
 
-
+//meant to be polled contantly, 
+//send clk bytes in a steady stream based on midi period untill remiaining bytes = 0
 void do_without_delay (){
-  
-  unsigned long currentMicros = micros();
-  if (currentMicros - midiEdgeLastMicros >= midiPeriodMicros) {
-    // save the last time you blinked the LED
-    midiEdgeLastMicros = currentMicros;
-    //do something here
-    Serial.write(MIDI_SYSRT_CLK);
-    //Serial.println("currentMicros");
-    //Serial.println("MIDI OUT NOW !!!!!!!!!!!!!!!");
-
-    //Serial.println(currentMicros);
-  }
-
+    
+    unsigned long currentMicros = micros();
+    if (midiLeftThisPulse >0 && currentMicros - midiEdgeLastMicros >= midiPeriodMicros) {
+        Serial.write(MIDI_SYSRT_CLK);    
+    
+        midiEdgeLastMicros = currentMicros;
+    
+        midiLeftThisPulse = midiLeftThisPulse -1;
+    }
 }
